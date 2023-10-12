06cd0233ce48fe8a2ae5ff3bbd7a6e7d9ee5ed4b
abhishekkaundal0052 <kaundal.abhishek0052@gmail.com>
2023-10-12 22:01:08 +0530

    2nd page

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-let welcome = document.getElementById("welcome")
-window.addEventListener('scroll', () => {
-    let value = window.scrollY;
-    text.style.marginTop = value * 2.5 +'px';
+// let welcome = document.getElementById("welcome")
+// window.addEventListener('scroll', () => {
+//     let value = window.scrollY;
+//     text.style.marginTop = value * 2.5 +'px';
+// }
+// );
+
+document.getElementById("welcome").onclick = () =>{
+    document.body.style.backgroundImage = "none";
 }
-);
