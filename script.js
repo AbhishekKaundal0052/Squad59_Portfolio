29db420cb49354dc138b323353a2c9c7fdbf57a7
abhishekkaundal0052 <kaundal.abhishek0052@gmail.com>
2023-10-13 18:37:22 +0530

    js

--- script.js
+++ script.js
@@ -4,7 +4,21 @@
 //     text.style.marginTop = value * 2.5 +'px';
 // }
 // );
-
-document.getElementById("welcome").onclick = () =>{
-    document.body.style.backgroundImage = "none";
+const blur = document.getElementById("info")
+var click  = 0
+function toggle() {
+    if (click === 0){
+        blur.style.filter = "blur(15px)"
+        click = 1
+    }
+    else if(click === 1){
+        blur.style.filter = "blur(0px)"
+        click = 0
+    }
+    
 }
+    var popup = document.getElementsByClassName("popuptext")
+    function information(){
+    
+}
+
